f600e2855fe1ac4b16f572a448a2eb597ccfdfc1
Garrett Estey <GDEstey@student.fullsail.edu>
2020-02-18 16:04:11 -0500

    Made the waves shader look like ocean waves. Will use this later... hopefully.

--- EsteyGarrettPPIV/BirdGraphics/Shaders/VS_Waves.hlsl
+++ EsteyGarrettPPIV/BirdGraphics/Shaders/VS_Waves.hlsl
@@ -4,7 +4,7 @@ PS_INPUT main(VS_INPUT input)
 {
     PS_INPUT output = (PS_INPUT) 0;
     output.pos = mul(input.pos, World);
-	output.pos = mul(output.pos, float4x4(float4(1.0f, 0.0f, 0.0f, 0.0f), float4(0.0f, 1.0f, 0.0f, 0.0f), float4(0.0f, 0.0f, 1.0f, 0.0f), float4(0.0f, (sin(time.x) * sin(input.pos.x) * 0.1), 0.0f, 1.0f)));
+	output.pos = mul(output.pos, float4x4(float4(1.0f, 0.0f, 0.0f, 0.0f), float4(0.0f, 1.0f, 0.0f, 0.0f), float4(0.0f, 0.0f, 1.0f, 0.0f), float4(0.0f, (sin(input.pos.x + (time.x * 2.0f)) * 0.1), 0.0f, 1.0f)));
     output.wPos = output.pos;
     output.pos = mul(output.pos, View);
     output.pos = mul(output.pos, Projection);
